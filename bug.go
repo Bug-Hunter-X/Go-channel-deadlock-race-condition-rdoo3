5ab13c443c95bc79aa30284e07e31291da2e09bb
BugHunter-X <askzenai@gmail.com>
2025-01-11 13:08:06 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,34 @@
+```go
+package main
+
+import (
+
+        "fmt"
+        "sync"
+)
+
+func main() {
+        var wg sync.WaitGroup
+        
+        ch := make(chan int)
+        
+        wg.Add(1)
+        go func() {
+                defer wg.Done()
+                for i := 0; i < 10; i++ {
+                        ch <- i
+                }
+                close(ch)
+        }()
+
+        wg.Add(1)
+        go func() {
+                defer wg.Done()
+                for i := range ch {
+                        fmt.Println(i)
+                }
+        }()
+
+        wg.Wait()
+} 
+```
